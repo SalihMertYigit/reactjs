04ff5bc11149176b74025012c953ca6c311a5481
SalihMertYigit <102948402+SalihMertYigit@users.noreply.github.com>
2022-05-19 21:52:40 +0300

    Initial commit

---
+++ react-frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import UserComponent from './component/UserComponent';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>selam</h1> 
+     <UserComponent/>
+     
+    </div>
+  );
+}
+
+export default App;
